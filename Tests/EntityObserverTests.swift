91c4dc7ae91a4496ca76ecc12718aa5d064e716f
Alessandro Marzoli <alessandromarzoli@gmail.com>
2018-02-24 13:56:43 +0100

    added another test for the onSave event

--- Tests/EntityObserverTests.swift
+++ Tests/EntityObserverTests.swift
@@ -469,6 +469,81 @@ class EntityObserverTests: XCTestCase {
     waitForExpectations(timeout: 10)
   }
 
+  func testRelationshipUpdatedOnSaveEvent() throws {
+    let stack = CoreDataStack.stack()
+    let context = stack.mainContext
+    let observedEvent = ObserverFrequency.onSave
+    let expectation1 = expectation(description: "\(#function)\(#line)")
+
+    let sportCar1 = SportCar(context: context)
+    sportCar1.maker = "McLaren"
+    sportCar1.model = "570GT"
+    sportCar1.numberPlate = "203"
+
+    let sportCar2 = SportCar(context: context)
+    sportCar2.maker = "Lamborghini "
+    sportCar2.model = "Aventador LP750-4"
+    sportCar2.numberPlate = "204"
+
+    let expensiveSportCar1 = ExpensiveSportCar(context: context)
+    expensiveSportCar1.maker = "BMW"
+    expensiveSportCar1.model = "M6 Coupe"
+    expensiveSportCar1.numberPlate = "300"
+    expensiveSportCar1.isLimitedEdition = true
+
+    let car1 = Car(context: context)
+    car1.maker = "FIAT"
+    car1.model = "Panda"
+    car1.numberPlate = "1"
+
+    let person1 = Person(context: context)
+    person1.firstName = "Edythe"
+    person1.lastName = "Moreton"
+
+    try context.save()
+
+    let entityObserver = EntityObserver<SportCar>(context: context, frequency: observedEvent)
+
+    let delegate = DummyEntityObserverDelegate<SportCar>()
+    delegate.onInserted = { inserted, event, observer in
+      XCTFail("There shouldn't be inserted objects.")
+    }
+
+    delegate.onDeleted = { deleted, event, observer in
+      XCTFail("There shouldn't be deleted objects.")
+    }
+
+    delegate.onUpdated = { updated, event, observer in
+      XCTAssertTrue(observer === entityObserver)
+      XCTAssertEqual(observedEvent, event)
+      XCTAssertEqual(updated.count, 2)
+
+      for car in updated {
+        XCTAssertTrue(car.owner === person1)
+      }
+
+      expectation1.fulfill()
+    }
+
+    delegate.onRefreshed = { refreshed, event, observer in
+      XCTFail("There shouldn't be refreshed objects.")
+    }
+
+    delegate.onInvalidated = { invalidated, event, observer in
+      XCTFail("There shouldn't be invalidated objects.")
+    }
+
+    let anyDelegate = AnyEntityObserverDelegate(delegate)
+    entityObserver.delegate = anyDelegate
+
+    person1.cars = [sportCar1, sportCar2]
+    expensiveSportCar1.numberPlate = expensiveSportCar1.numberPlate + " Updated"
+
+    try context.save()
+
+    waitForExpectations(timeout: 10)
+  }
+
   // MARK: - Change and Save Events
 
   // MARK: - Utils
