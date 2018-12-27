9cf527a061577b1b49292a2b38f43ef837ae86f5
Alessandro Marzoli <a.marzoli@ambrogio.com>
2018-12-27 09:48:31 +0100

    better tests

--- Tests/NSEntityDescriptionUtilsTests.swift
+++ Tests/NSEntityDescriptionUtilsTests.swift
@@ -42,15 +42,17 @@ final class NSEntityDescriptionUtilsTests: CoreDataPlusTestCase {
     let context = container.viewContext
 
     do {
+      guard Car.entity().name != nil else { return }
       let expensiveCar = ExpensiveSportCar(context: context)
       let topMostAncestorEntity = expensiveCar.entity.topMostEntity
-      XCTAssertTrue(topMostAncestorEntity == Car.entity())
+      XCTAssertTrue(topMostAncestorEntity == Car.entity(), "\(topMostAncestorEntity) should be a Car entity.")
     }
 
     do {
+      guard Car.entity().name != nil else { return }
       let car = Car(context: context)
       let topMostAncestorEntity = car.entity.topMostEntity
-      XCTAssertTrue(topMostAncestorEntity == Car.entity())
+      XCTAssertTrue(topMostAncestorEntity == Car.entity(), "\(topMostAncestorEntity) should be a Car entity.")
     }
 
   }
