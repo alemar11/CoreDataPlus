542e4bf5c50a9d510678891c845f6279876be957
Alessandro Marzoli <alessandromarzoli@gmail.com>
2020-05-26 08:35:59 +0200

    TODO

--- Sources/NSFetchRequestResult+CoreData.swift
+++ Sources/NSFetchRequestResult+CoreData.swift
@@ -21,6 +21,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+// Consider includesPendingChanges in 3.0.0
+
 import CoreData
 
 extension NSFetchRequestResult where Self: NSManagedObject {
