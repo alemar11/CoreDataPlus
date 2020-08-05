85bc26b98dfba8ec9178e252e1a1620b673d811a
Alessandro Marzoli <alessandromarzoli@gmail.com>
2020-08-05 14:16:13 +0200

    XCTUnwrap

--- Tests/NSSetCoreDataTests.swift
+++ Tests/NSSetCoreDataTests.swift
@@ -16,11 +16,7 @@ final class NSSetCoreDataTests: CoreDataPlusInMemoryTestCase {
     let request = Person.newFetchRequest()
     request.returnsObjectsAsFaults = true
     let foundPerson = try Person.materializedObjectOrFetch(in: context, where: NSPredicate(format: "\(#keyPath(Person.firstName)) == %@ AND \(#keyPath(Person.lastName)) == %@", "Theodora", "Stone"))
-    //let person = try XCTUnwrap(foundPerson) // TODO Swift 5.2
-    guard let person = foundPerson else {
-      XCTAssertNotNil(foundPerson)
-      return
-    }
+    let person = try XCTUnwrap(foundPerson)
     context.refreshAllObjects()
 
     let initialFaultsCount = person.cars?.filter { object in
@@ -55,11 +51,8 @@ final class NSSetCoreDataTests: CoreDataPlusInMemoryTestCase {
     let request = Person.newFetchRequest()
     request.returnsObjectsAsFaults = true
     let foundPerson = try Person.materializedObjectOrFetch(in: context, where: NSPredicate(format: "\(#keyPath(Person.firstName)) == %@ AND \(#keyPath(Person.lastName)) == %@", "Theodora", "Stone"))
-    //let person = try XCTUnwrap(foundPerson) // TODO Swift 5.2
-    guard let person = foundPerson else {
-      XCTAssertNotNil(foundPerson)
-      return
-    }
+    let person = try XCTUnwrap(foundPerson)
+
     context.refreshAllObjects()
     XCTAssertEqual(person.cars?.count ?? 0, 100)
     person.cars?.deleteManagedObjects()
