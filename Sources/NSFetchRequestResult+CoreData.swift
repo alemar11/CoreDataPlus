df8bd7d69fe1c022cf62d4e166dcdbae895edab3
Alessandro Marzoli <alessandromarzoli@gmail.com>
2020-08-21 13:07:25 +0200

    polish entityName

--- Sources/NSFetchRequestResult+CoreData.swift
+++ Sources/NSFetchRequestResult+CoreData.swift
@@ -7,16 +7,25 @@ extension NSFetchRequestResult where Self: NSManagedObject {
   ///
   /// The entity name.
   public static var entityName: String {
-    if let name = entity().name {
-      return name
+    guard let name = entity().name else {
+      fatalError("\(String(describing: Self.self)) doesn't have an entity name.")
     }
-    // Attention: sometimes entity() returns nil due to a CoreData bug occurring in the Unit Test targets or when Generics are used.
-    // https://forums.developer.apple.com/message/203409#203409
-    // https://stackoverflow.com/questions/37909392/exc-bad-access-when-calling-new-entity-method-in-ios-10-macos-sierra-core-da
-    // https://stackoverflow.com/questions/43231873/nspersistentcontainer-unittests-with-ios10/43286175
-    // https://www.jessesquires.com/blog/swift-coredata-and-testing/
-    // https://github.com/jessesquires/rdar-19368054
-    return String(describing: Self.self)
+    return name
+
+    // TODO: The bug seems fixed (Xcode 12b5)
+    // alternative solution, instead of a fatalError, we could use and assertion and return
+    // String(describing: Self.self) as fallback (like in the previous implementation)
+
+//    if let name = entity().name {
+//      return name
+//    }
+//    // Attention: sometimes entity() returns nil due to a CoreData bug occurring in the Unit Test targets or when Generics are used.
+//    // https://forums.developer.apple.com/message/203409#203409
+//    // https://stackoverflow.com/questions/37909392/exc-bad-access-when-calling-new-entity-method-in-ios-10-macos-sierra-core-da
+//    // https://stackoverflow.com/questions/43231873/nspersistentcontainer-unittests-with-ios10/43286175
+//    // https://www.jessesquires.com/blog/swift-coredata-and-testing/
+//    // https://github.com/jessesquires/rdar-19368054
+//    return String(describing: Self.self)
   }
 
   // MARK: - Fetch
