fd1a4555b818733ae0399e7b2477d3a7114a739d
Alessandro Marzoli <alessandromarzoli@gmail.com>
2020-11-14 11:06:06 +0100

    notes

--- Package.swift
+++ Package.swift
@@ -16,14 +16,22 @@ let package = Package(
                     exclude: [
                       "TestPlans",
                       "Resources/SampleModel/Fixtures/README.md",
-                      //"Resources/SampleModel/Fixtures/SampleModel.momd",
-                      //"Resources/SampleModel/Fixtures/V2toV3.cdm"
+
+                      // When running tests from Xcode, these resources needs to be excluded
+                      // because they are automatically created by Xcode; viceversa when running
+                      // tests from terminal these lines should be commented out.
+                      "Resources/SampleModel/Fixtures/SampleModel.momd",
+                      "Resources/SampleModel/Fixtures/V2toV3.cdm"
                     ],
                     resources: [
                       .copy("Resources/SampleModel/Fixtures/SampleModelV1.sqlite"),
                       .copy("Resources/SampleModel/Fixtures/SampleModelV2.sqlite"),
-//                      .copy("Resources/SampleModel/Fixtures/SampleModel.momd"),
-//                      .copy("Resources/SampleModel/Fixtures/V2toV3.cdm"),
+                      
+                      // When running tests from terminal, these resources needs to be copied in the test bundle,
+                      // viceversa when running tests from Xcode these lines should be commented out
+                      // because Xcode will create automatically these binaries.
+                      //.copy("Resources/SampleModel/Fixtures/SampleModel.momd"),
+                      //.copy("Resources/SampleModel/Fixtures/V2toV3.cdm"),
                     ]
         ),
     ],
--- Tests/Resources/SampleModel/Fixtures/README.md
+++ Tests/Resources/SampleModel/Fixtures/README.md
@@ -1,15 +1,18 @@
-To run swift tests from terminal we need to have compiled versions of the model and mapping models
-that gets copied in the resources bundle during the build phase.
+How to run `swift test` from terminal.
 
-These are the files currently used:
-
-- `SampleModel.momd`
-- `V2toV3.cdm`
-
-Apple recommends you create a Resources folder under the Sources folder but it’s not required.
-Xcode knows how to handle common Apple resource types out-of-the box. These include (not an exhaustive list):
+When you open a Swift package with Xcode, Xcode knows how to handle common Apple resource types out-of-the box. These include (not an exhaustive list):
 
 - Asset Catalogs (.xcassets)
 - Storyboards and NIBs (.storyboard, .xib, .nib)
 - Core Data models (.xcdatamodel, xcmappingmodel)
 - Localization folders (.lproj)
+
+That means Xcode will compile `SampleModel.xcdatamodeld` into `SampleModel.md` and  `V2toV3.xcmappingmodel` into `V2toV3.cdm` and will copy them in the Resources folder of the test bundle automatically.
+
+When using the terminal, though, we need to have already compiled versions of the model and mapping models and copy them in the resources bundle during the build phase.
+That's why the `Fixtures` folder contains these binaries:
+
+- `SampleModel.momd`
+- `V2toV3.cdm`
+
+The main problem to have tests working from both Xcode and terminal is that, when building from Xcode we need to exclude the compiled binaries described above because Xcode will create them automatically for us, while when building from termianl these binaries must be inclueded and copied.
