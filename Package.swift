42aaf924989eac022d964fd8456593f7c2d086d5
Alessandro Marzoli <alessandromarzoli@gmail.com>
2020-11-14 12:10:33 +0100

    cleaning

--- Package.swift
+++ Package.swift
@@ -3,15 +3,20 @@
 import PackageDescription
 import Foundation
 
-let isTerminal: Bool = {
+let isRunningTestsFromCommandLine: Bool = {
   return ProcessInfo.processInfo.environment["XPC_SERVICE_NAME"] == "0"
 }()
 
+var excluded = [
+  "TestPlans",
+  "Resources/SampleModel/Fixtures/README.md"
+]
+var resources: [Resource] = [
+  .copy("Resources/SampleModel/Fixtures/SampleModelV1.sqlite"),
+  .copy("Resources/SampleModel/Fixtures/SampleModelV2.sqlite")
+]
 
-var excluded = ["TestPlans", "Resources/SampleModel/Fixtures/README.md"]
-var resources: [Resource] = [.copy("Resources/SampleModel/Fixtures/SampleModelV1.sqlite"),
-                           .copy("Resources/SampleModel/Fixtures/SampleModelV2.sqlite"),]
-if isTerminal {
+if isRunningTestsFromCommandLine {
   // When running tests from terminal, these resources needs to be copied in the test bundle,
   // viceversa when running tests from Xcode, Xcode will create automatically these binaries automaticcally.
   resources.append(contentsOf: [
