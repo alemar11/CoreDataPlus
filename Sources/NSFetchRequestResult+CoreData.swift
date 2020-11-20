ada7dff7944a7c5e772a001db5590b43ac2b7b55
Alessandro Marzoli <alessandromarzoli@gmail.com>
2020-11-20 17:56:14 +0100

    note

--- Sources/NSFetchRequestResult+CoreData.swift
+++ Sources/NSFetchRequestResult+CoreData.swift
@@ -11,7 +11,7 @@ extension NSFetchRequestResult where Self: NSManagedObject {
       return name
     }
     // Attention: sometimes entity() returns nil due to a CoreData bug occurring in the Unit Test targets or when Generics are used.
-    // The bug seems fixed on Xcode 12
+    // The bug seems fixed on Xcode 12 but having a fallback option never hurts.
     // https://forums.developer.apple.com/message/203409#203409
     // https://stackoverflow.com/questions/37909392/exc-bad-access-when-calling-new-entity-method-in-ios-10-macos-sierra-core-da
     // https://stackoverflow.com/questions/43231873/nspersistentcontainer-unittests-with-ios10/43286175
