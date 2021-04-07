944e3365917aba2432e3450bc3bc1b37343bf29b
Alessandro Marzoli <alessandromarzoli@gmail.com>
2021-04-07 11:39:31 +0200

    added a BaseEntity for testing purposes

--- Tests/Resources/SampleModel/Entities/Car.swift
+++ Tests/Resources/SampleModel/Entities/Car.swift
@@ -4,17 +4,25 @@ import Foundation
 import CoreData
 import CoreDataPlus
 
+// Entity hierarchy vs Class hierarchy
+//
+// Managed object models offer the possibility to create entity hierarchies (i.e. we can specify an entity as the parent of another entity).
+// This might sound good if our entities share some common attributes but it’s rarely what you would want to do.
+// What happens behind the scenes is that Core Data stores all entities with the same parent entity in the same table. This quickly creates tables with a large number of attributes, slowing down performance.
+// Often the purpose of creating an entity hierarchy is solely to create a class hierarchy, so that we can put code shared between multiple entities into the base class.
+// This (Class hierarchy) gives us the benefit of being able to move common code into the base class without the performance overhead of storing all entities in a single table.
+// ⚠️ We cannot create class hierarchies deviating from the entity hierarchy with Xcode’s managed object generator though.
+public class BaseEntity: NSManagedObject, DelayedDeletable {
+  @NSManaged public var markedForDeletionAsOf: Date?
+}
+
 @objc(Car)
-public class Car: NSManagedObject {
+public class Car: BaseEntity {
   @NSManaged public var maker: String?
   @NSManaged public var model: String?
   @NSManaged public var numberPlate: String!
   @NSManaged public var owner: Person?
   @NSManaged public var currentDrivingSpeed: Int // transient property
-}
-
-extension Car: DelayedDeletable {
-  @NSManaged public var markedForDeletionAsOf: Date?
 }
 
 @objc(SportCar)
