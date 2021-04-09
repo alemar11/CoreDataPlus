114f7c377fb24a8513292102844963ae022ea4e8
Alessandro Marzoli <alessandromarzoli@gmail.com>
2021-04-09 16:15:28 +0200

    added DataTransfomer and tests

--- Tests/TransformerTests.swift
+++ Tests/TransformerTests.swift
@@ -39,8 +39,24 @@ class TransformerTests: CoreDataPlusOnDiskTestCase {
     let color = try XCTUnwrap(firstCar.color)
     XCTAssertEqual(color.name, "red")
   }
+  
+  func testSaveAndFetchTransformableValueUsingCustomTransformer() throws {
+    let context = container.viewContext
+    let car = Car(context: context)
+    car.maker = "FIAT"
+    car.model = "Panda"
+    car.numberPlate = "1"
+    car.color = Color(name: "red")
+    try context.save()
+    context.reset()
 
-  func testUnregister() {
+    let firstCar = try XCTUnwrap(try Car.fetchOne(in: context, where: NSPredicate(value: true)))
+
+    let color = try XCTUnwrap(firstCar.color)
+    XCTAssertEqual(color.name, "red")
+  }
+
+  func testTransformerUnregister() {
     XCTAssertFalse(Foundation.ValueTransformer.valueTransformerNames().contains(Transformer<Dummy>.transformerName))
     Transformer<Dummy>.register()
     XCTAssertTrue(Foundation.ValueTransformer.valueTransformerNames().contains(Transformer<Dummy>.transformerName))
@@ -48,5 +64,73 @@ class TransformerTests: CoreDataPlusOnDiskTestCase {
     XCTAssertFalse(Foundation.ValueTransformer.valueTransformerNames().contains(Transformer<Dummy>.transformerName))
   }
   
+  func testDataTransformerUnregister() {
+    XCTAssertFalse(Foundation.ValueTransformer.valueTransformerNames().contains(DataTransformer<Dummy>.transformerName))
+    DataTransformer<Dummy>.register {
+      guard let color = $0 else { return nil }
+      return try? NSKeyedArchiver.archivedData(withRootObject: color, requiringSecureCoding: true)
+    } reverseTransform: {
+      guard let data = $0 else { return nil }
+      return try? NSKeyedUnarchiver.unarchivedObject(ofClass: Dummy.self, from: data)
+    }
+    XCTAssertTrue(Foundation.ValueTransformer.valueTransformerNames().contains(DataTransformer<Dummy>.transformerName))
+    Transformer<Dummy>.unregister()
+    XCTAssertFalse(Foundation.ValueTransformer.valueTransformerNames().contains(DataTransformer<Dummy>.transformerName))
+  }
+    
+  func testTransformer() throws {
+    let transformer = Transformer<Color>()
+    let data = transformer.reverseTransformedValue(Color(name: "green"))
+    XCTAssertNotNil(data)
+    let expectedColor = transformer.transformedValue(data) as? Color
+    XCTAssertNotNil(expectedColor)
+    let name = try XCTUnwrap(expectedColor?.name)
+    XCTAssertEqual(name, "green")
+  }
   
+  func testCustomTransformer() throws {
+    let transformer = DataTransformer<Color> { color -> Data? in
+      guard let color = color else { return nil }
+      return try? NSKeyedArchiver.archivedData(withRootObject: color, requiringSecureCoding: true)
+    } reverseTransform: { data -> Color? in
+      guard let data = data else { return nil }
+      return try? NSKeyedUnarchiver.unarchivedObject(ofClass: Color.self, from: data)
+    }
+    let data = transformer.transformedValue(Color(name: "green"))
+    XCTAssertNotNil(data)
+    let expectedColor = transformer.reverseTransformedValue(data) as? Color
+    XCTAssertNotNil(expectedColor)
+    let name = try XCTUnwrap(expectedColor?.name)
+    XCTAssertEqual(name, "green")
+  }
+  
+  func testDataTransformerWithNSArray() throws {
+    let transformer = DataTransformer<NSArray> { array -> Data? in
+      guard let array = array else { return nil }
+      return try? NSKeyedArchiver.archivedData(withRootObject: array, requiringSecureCoding: true)
+    } reverseTransform: { data -> NSArray? in
+      guard let data = data else { return nil }
+      return try? NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(data) as? NSArray
+    }
+
+    let array = NSMutableArray()
+    array.add(1)
+    array.add(Color(name: "green"))
+    array.add(Dummy(name: "dummy1"))
+    array.add([Dummy(name: "dummy2"), Dummy(name: "dummy2")])
+    let data = transformer.transformedValue(NSArray(array: array))
+    XCTAssertNotNil(data)
+    
+    let expectedTransformedValue = transformer.reverseTransformedValue(data)
+    let expectedNSArray = try XCTUnwrap(expectedTransformedValue as? NSArray)
+    let first = try XCTUnwrap(expectedNSArray.object(at: 0) as? Int)
+    let second = try XCTUnwrap(expectedNSArray.object(at: 1) as? Color)
+    let third = try XCTUnwrap(expectedNSArray.object(at: 2) as? Dummy)
+    let fourth = try XCTUnwrap(expectedNSArray.object(at: 3) as? [Dummy])
+    XCTAssertEqual(first, 1)
+    XCTAssertEqual(second.name, "green")
+    XCTAssertEqual(third.name, "dummy1")
+    XCTAssertEqual(fourth.first?.name, "dummy2")
+    XCTAssertEqual(fourth.last?.name, "dummy2")
+  }
 }
