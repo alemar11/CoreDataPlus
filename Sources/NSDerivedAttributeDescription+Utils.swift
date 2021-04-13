d12e2048546c5e98522f0216e4782a5af4914f88
Alessandro Marzoli <alessandromarzoli@gmail.com>
2021-04-13 14:01:44 +0200

    refactor

--- Sources/NSDerivedAttributeDescription+Utils.swift
+++ Sources/NSDerivedAttributeDescription+Utils.swift
@@ -3,7 +3,6 @@
 
 import CoreData
 
-
 @available(iOS 13.0, iOSApplicationExtension 13.0, macCatalyst 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, *)
 extension NSDerivedAttributeDescription {
   /// Creates a new `NSDerivedAttributeDescription` instance.
