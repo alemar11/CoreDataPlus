64be13620bfdf7245336ad8a166b15fd9a278033
Alessandro Marzoli <alessandromarzoli@gmail.com>
2021-04-16 15:57:33 +0200

    --wip-- [skip ci]

--- Tests/ProgrammaticMigrationTests.swift
+++ Tests/ProgrammaticMigrationTests.swift
@@ -4,118 +4,49 @@ import XCTest
 @testable import CoreDataPlus
 
 @available(iOS 13.0, iOSApplicationExtension 13.0, macCatalyst 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, *)
-final class ModelBuilderTests: OnDiskWithProgrammaticallyModelTestCase {
-  func testSetup() throws {
-    let context = container.viewContext
-    context.fillWithSampleData2()
-    try context.save()
-    context.reset()
+final class ProgrammaticMigrationTests: XCTestCase {
+  
+  func testInferringMappingModelFromV1toV2() throws {
+    let mappingModel = SampleModel2.SampleModel2Version.version1.inferredMappingModelToNextModelVersion()
+    let mappings = try XCTUnwrap(mappingModel?.entityMappings)
+    let authorMappingModel = try XCTUnwrap(mappings.first(where:{ $0.sourceEntityName == "Author" }))
+    XCTAssertEqual(authorMappingModel.mappingType, .transformEntityMappingType)
     
-    let books = try V1.Book.fetch(in: context)
-    XCTAssertEqual(books.count, 52)
+    let bookMappingModel = try XCTUnwrap(mappings.first(where:{ $0.sourceEntityName == "Book" }))
+    XCTAssertEqual(bookMappingModel.mappingType, .transformEntityMappingType)
     
-    let authors = try V1.Author.fetch(in: context)
-    XCTAssertEqual(authors.count, 2)
+    do {
+      let mappingProperties = try XCTUnwrap(authorMappingModel.mappingProperties)
+      XCTAssertEqual(mappingProperties.mappedProperties.count, 3)
+      XCTAssertTrue(mappingProperties.mappedProperties.contains("age"))
+      XCTAssertTrue(mappingProperties.mappedProperties.contains("alias"))
+      XCTAssertTrue(mappingProperties.mappedProperties.contains("books"))
+      
+      XCTAssertTrue(mappingProperties.addedProperties.isEmpty)
+      
+      XCTAssertEqual(mappingProperties.removedProperties.count, 1)
+      XCTAssertTrue(mappingProperties.removedProperties.contains("siteURL"))
+    }
     
-    let fetchedAuthor = try V1.Author.fetch(in: context) { $0.predicate = NSPredicate(format: "%K == %@", #keyPath(V1.Author.alias), "Alessandro") }.first
-    
-    let author = try XCTUnwrap(fetchedAuthor)
-    let feedbacks = try XCTUnwrap(author.feedbacks)
-    XCTAssertEqual(feedbacks.map({ $0.rating }), [3.5, 4.2, 4.3])
-    
-    XCTAssertEqual(author.favFeedbacks?.count, 2)
+    do {
+      XCTAssertNotNil(bookMappingModel.attributeMappings?.first(where: { $0.name == "frontCover" })) // this is as far I can go
+      let mappingProperties = try XCTUnwrap(bookMappingModel.mappingProperties)
+      
+      XCTAssertEqual(mappingProperties.mappedProperties.count, 8)
+      XCTAssertTrue(mappingProperties.mappedProperties.contains("frontCover"))
+      XCTAssertFalse(mappingProperties.mappedProperties.contains("cover"))
+      XCTAssertTrue(mappingProperties.addedProperties.isEmpty)
+      XCTAssertTrue(mappingProperties.removedProperties.isEmpty)
+    }
   }
   
-  func testTweakFetchedPropertyAtRuntime() throws {
-    let context = container.viewContext
-    context.fillWithSampleData2()
-    try context.save()
-    context.reset()
-    
-    let fetchedAuthor = try V1.Author.fetch(in: context) { $0.predicate = NSPredicate(format: "%K == %@", #keyPath(V1.Author.alias), "Alessandro") }.first
-    
-    let author = try XCTUnwrap(fetchedAuthor)
-    XCTAssertEqual(author.favFeedbacks?.count, 2)
-    
-    let fetchedProperties = V1.Author.entity().properties.compactMap { $0 as? NSFetchedPropertyDescription }
-    let favFeedbacksFetchedProperty = try XCTUnwrap(fetchedProperties.filter ({ $0.name == V1.Author.FetchedProperty.favFeedbacks }).first)
-    
-    // During the creation of the model, an key 'search' with value 'great' has been added to the fetched property
-    // If we change its value at runtime, the result will reflect that.
-    favFeedbacksFetchedProperty.userInfo?["search"] = "interesting"
-    XCTAssertEqual(author.favFeedbacks?.count, 1)
+  func test_1() {
+    //XCTAssertEqual(AuthorV2.entity().name, AuthorV2.entityName)
   }
-}
-
-@available(OSX 10.15, *)
-final class _ProgrammaticMigrationTests: XCTestCase {
-  
-  //  func createSampleVersion1() throws -> URL {
-  //    try autoreleasepool {
-  //    let model = V1.makeManagedObjectModel()
-  //    let container = NSPersistentContainer(name: "SampleModel2", managedObjectModel: model)
-  //    let url = URL.newDatabaseURL(withID: UUID())
-  //    container.persistentStoreDescriptions[0].shouldAddStoreAsynchronously = false
-  //    container.persistentStoreDescriptions[0].url = url
-  //    container.loadPersistentStores { (description, error) in
-  //      XCTAssertNil(error)
-  //
-  //      let context = container.viewContext
-  //      context.fillWithSampleData2()
-  //      do {
-  //        try context.save()
-  //      } catch {
-  //        XCTFail(error.localizedDescription)
-  //      }
-  //    }
-  //      if let store = container.persistentStoreCoordinator.persistentStores.first {
-  //        try container.persistentStoreCoordinator.remove(store)
-  //      }
-  //
-  //    return url
-  //    }
-  //  }
-  
-  //  func testInferringMappingModelFromV1toV2() throws {
-  //    let mappingModel = SampleModel2.SampleModel2Version.version1.inferredMappingModelToNextModelVersion()
-  //    let mappings = try XCTUnwrap(mappingModel?.entityMappings)
-  //    let authorMappingModel = try XCTUnwrap(mappings.first(where:{ $0.sourceEntityName == "Author" }))
-  //    XCTAssertEqual(authorMappingModel.mappingType, .transformEntityMappingType)
-  //
-  //    let bookMappingModel = try XCTUnwrap(mappings.first(where:{ $0.sourceEntityName == "Book" }))
-  //    XCTAssertEqual(bookMappingModel.mappingType, .transformEntityMappingType)
-  //
-  //    do {
-  //      let mappingProperties = try XCTUnwrap(authorMappingModel.mappingProperties)
-  //      XCTAssertEqual(mappingProperties.mappedProperties.count, 3)
-  //      XCTAssertTrue(mappingProperties.mappedProperties.contains("age"))
-  //      XCTAssertTrue(mappingProperties.mappedProperties.contains("alias"))
-  //      XCTAssertTrue(mappingProperties.mappedProperties.contains("books"))
-  //
-  //      XCTAssertTrue(mappingProperties.addedProperties.isEmpty)
-  //
-  //      XCTAssertEqual(mappingProperties.removedProperties.count, 1)
-  //      XCTAssertTrue(mappingProperties.removedProperties.contains("siteURL"))
-  //    }
-  
-  //    do {
-  //      XCTAssertNotNil(bookMappingModel.attributeMappings?.first(where: { $0.name == "frontCover" })) // this is as far I can go
-  //      let mappingProperties = try XCTUnwrap(bookMappingModel.mappingProperties)
-  //
-  //      XCTAssertEqual(mappingProperties.mappedProperties.count, 8)
-  //      XCTAssertTrue(mappingProperties.mappedProperties.contains("frontCover"))
-  //      XCTAssertFalse(mappingProperties.mappedProperties.contains("cover"))
-  //      XCTAssertTrue(mappingProperties.addedProperties.isEmpty)
-  //      XCTAssertTrue(mappingProperties.removedProperties.isEmpty)
-  //    }
-  //  }
   
   func testMigrationFromV1ToV2() throws {
-    #warning("This fails unless the model is loaded")
-    //XCTAssertEqual(AuthorV2.entity().name, AuthorV2.entityName)
-    
     let url = URL.newDatabaseURL(withID: UUID())
-
+    
     let options = [
       NSMigratePersistentStoresAutomaticallyOption: true,
       NSInferMappingModelAutomaticallyOption: false,
@@ -133,7 +64,6 @@ final class _ProgrammaticMigrationTests: XCTestCase {
     coordinator.addPersistentStore(with: description) { (description, error) in
       XCTAssertNil(error)
     }
-    //try coordinator!.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: url, options: SampleModel2.SampleModel2Version.version1.options)
     
     let context = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
     context.persistentStoreCoordinator = coordinator
@@ -145,16 +75,7 @@ final class _ProgrammaticMigrationTests: XCTestCase {
     })
     
     // Migration
-    //try CoreDataPlus.Migration.migrateStore(at: url, targetVersion: SampleModel2.SampleModel2Version.version2, enableWALCheckpoint: true)
-    try CoreDataPlus.Migration.migrateStore(from: url,
-                                            sourceOptions: options,
-                                            to: url,
-                                            targetOptions: options,
-                                            targetVersion: SampleModel2.SampleModel2Version.version2,
-                                            deleteSource: false,
-                                            enableWALCheckpoint: true,
-                                            progress: nil)
-    
+    try CoreDataPlus.Migration.migrateStore(at: url, options: options, targetVersion: SampleModel2.SampleModel2Version.version2, enableWALCheckpoint: true)
     
     // Validation
     let newManagedObjectModel = V2.makeManagedObjectModel()
@@ -172,8 +93,6 @@ final class _ProgrammaticMigrationTests: XCTestCase {
     authors.forEach { object in
       object.materialize()
       object.alias += "-"
-      print(object.feedbacks?.count)
-      //XCTAssertNil(object.value(forKey: #keyPath(V1.Author.siteURL)))
     }
     
     let bookRequest = NSFetchRequest<NSManagedObject>(entityName: "Book")
@@ -181,48 +100,18 @@ final class _ProgrammaticMigrationTests: XCTestCase {
     XCTAssertEqual(books.count, 52)
     books.forEach { object in
       object.materialize()
-      //print(object)
       XCTAssertNotNil(object.value(forKey: #keyPath(BookV2.frontCover)))
     }
-    
-    //print(authors.first?.age)
-    //    let personRequest = NSFetchRequest<NSFetchRequestResult>.init(entityName: "Person")
-    //          XCTAssertEqual(try! newManagedObjectContext.executeFetchRequest(personRequest).count, 2)
-    //    let teacherRequest = NSFetchRequest<NSFetchRequestResult>.init(entityName: "Teacher")
-    //          XCTAssertEqual(try! newManagedObjectContext.executeFetchRequest(teacherRequest).count, 1)
     
     let feedbacksCount = try FeedbackV2.count(in: newContext)
     XCTAssertEqual(feedbacksCount, 444)
     
     try! newContext.save()
     newContext._fix_sqlite_warning_when_destroying_a_store()
-    //try FileManager.default.removeItem(at: url)
+    try FileManager.default.removeItem(at: url)
   }
   
-  
-  //    func testMigrationFromV1toV2() throws {
-  //      // step 1: populate
-  //      let url = try createSampleVersion1()
-  //
-  //      try CoreDataPlus.Migration.migrateStore(at: url, targetVersion: SampleModel2.SampleModel2Version.version2)
-  //
-  //      let model = V2.makeManagedObjectModel()
-  //      let container = NSPersistentContainer(name: "SampleModel2", managedObjectModel: model)
-  //      container.persistentStoreDescriptions[0].url = url
-  //      container.loadPersistentStores { (_, error) in
-  //        XCTAssertNil(error)
-  //      }
-  //
-  //      let context = container.viewContext
-  //      let fr = V2.Author.fetchRequest()
-  //      //let fr = NSFetchRequest<NSFetchRequestResult>.init()
-  //      let res = try context.fetch(fr)
-  //      let authors = try V2.Author.fetch(in: context)
-  //      XCTAssertEqual(authors.count, 2)
-  //  }
-  
-  @available(OSX 10.15, *)
-  func testMigrationFromV1ToV2_Advanced() throws {
+  func testMigrationFromV1ToV2WithMultipleStores() throws {
     #warning("This fails unless the model is loaded")
     //XCTAssertEqual(AuthorV2.entity().name, AuthorV2.entityName)
     
@@ -232,11 +121,8 @@ final class _ProgrammaticMigrationTests: XCTestCase {
       NSPersistentHistoryTrackingKey: true, // cann't be changed once set to true
       NSPersistentHistoryTokenKey: true
     ]
-    
-    let cachesURL = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
-    let id = UUID()
-    let url = URL.newDatabaseURL(withID: id)
-    
+        
+    let url = URL.newDatabaseURL(withID: UUID())
     let oldManagedObjectModel = V1.makeManagedObjectModel()
     let coordinator = NSPersistentStoreCoordinator(managedObjectModel: oldManagedObjectModel)
     
@@ -273,9 +159,10 @@ final class _ProgrammaticMigrationTests: XCTestCase {
     
     let part1 = try XCTUnwrap(newCoordinator.persistentStores.first { $0.configurationName == V2.Configurations.part1 })
     let part2 = try XCTUnwrap(newCoordinator.persistentStores.first { $0.configurationName == V2.Configurations.part2 })
-    
+
     let _authors = try AuthorV2.fetch(in: newContext)
     XCTAssertEqual(AuthorV2.entity().name, AuthorV2.entityName)
+    
     let authorRequest = NSFetchRequest<NSManagedObject>(entityName: "Author") as! NSFetchRequest<AuthorV2>
     let authors = try newContext.fetch(authorRequest)
     XCTAssertEqual(authors.count, 2)
@@ -317,7 +204,7 @@ final class _ProgrammaticMigrationTests: XCTestCase {
       let fetchedAuthor = try newContext.fetch(fetchedAuthorRequest).first
       
       let author = try XCTUnwrap(fetchedAuthor)
-      print(author.feedbacks?.count) // 6 -> 3 + 3
+      XCTAssertEqual(author.feedbacks?.count, 6) // 3 each store
     }
     
     do {
@@ -326,35 +213,13 @@ final class _ProgrammaticMigrationTests: XCTestCase {
       let fetchedAuthor = try newContext.fetch(fetchedAuthorRequest).first
       
       let author = try XCTUnwrap(fetchedAuthor)
-      print(author.feedbacks?.count) // 882 -> 441 + 441
+      XCTAssertEqual(author.feedbacks?.count, 882) // 441 each store
     }
     
     newContext._fix_sqlite_warning_when_destroying_a_store()
-    //try FileManager.default.removeItem(at: url)
-    //try FileManager.default.removeItem(at: urlPart2)
+    try FileManager.default.removeItem(at: url)
+    try FileManager.default.removeItem(at: urlPart2)
   }
-  
-  
-  //    func testMigrationFromV1toV2() throws {
-  //      // step 1: populate
-  //      let url = try createSampleVersion1()
-  //
-  //      try CoreDataPlus.Migration.migrateStore(at: url, targetVersion: SampleModel2.SampleModel2Version.version2)
-  //
-  //      let model = V2.makeManagedObjectModel()
-  //      let container = NSPersistentContainer(name: "SampleModel2", managedObjectModel: model)
-  //      container.persistentStoreDescriptions[0].url = url
-  //      container.loadPersistentStores { (_, error) in
-  //        XCTAssertNil(error)
-  //      }
-  //
-  //      let context = container.viewContext
-  //      let fr = V2.Author.fetchRequest()
-  //      //let fr = NSFetchRequest<NSFetchRequestResult>.init()
-  //      let res = try context.fetch(fr)
-  //      let authors = try V2.Author.fetch(in: context)
-  //      XCTAssertEqual(authors.count, 2)
-  //  }
 }
 
 extension NSEntityMapping {
