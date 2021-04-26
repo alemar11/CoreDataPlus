8eda48f38fac524842ecc0b86cbfcd9033dd54c3
Alessandro Marzoli <alessandromarzoli@gmail.com>
2021-04-26 10:38:24 +0200

    notes

--- Tests/Resources/SampleModel2/SampleModel2+V3.swift
+++ Tests/Resources/SampleModel2/SampleModel2+V3.swift
@@ -541,21 +541,50 @@ extension V3 {
 @objc(BookCoverToCoverMigrationPolicy)
 class BookCoverToCoverMigrationPolicy: NSEntityMigrationPolicy {
   override func createDestinationInstances(forSource sInstance: NSManagedObject, in mapping: NSEntityMapping, manager: NSMigrationManager) throws {
+    // This method is invoked by the migration manager on each source instance (as specified by the sourceExpression in the mapping)
+    // to create the corresponding destination instance(s).
+    // It also associates the source and destination instances by calling NSMigrationManager’s associate(sourceInstance:withDestinationInstance:for:) method.
     try super.createDestinationInstances(forSource: sInstance, in: mapping, manager: manager)
+
+    // If we don't call the super implementation we need to do the association programmatically like so:
+    // Note: since you already have a destinationInstance, you won't need to call anymore manager.destinationInstances(forEntityMappingName:sourceInstances:)
+//    let destinationInstance = NSEntityDescription.insertNewObject(forEntityName: mapping.destinationEntityName!, into: manager.destinationContext)
+//    let destinationInstanceKeys = destinationInstance.entity.attributesByName.keys
+//    destinationInstanceKeys.forEach { (key) in
+//      if let value = sInstance.value(forKey: key) {
+//        if let nsobject = value as? NSObject, nsobject.isEqual(NSNull()) {
+//          return
+//        }
+//        destinationInstance.setValue(value, forKey: key)
+//      }
+//    }
+//    manager.associate(sourceInstance: sInstance, withDestinationInstance: destinationInstance, for: mapping)
+
 
     guard let frontCover = sInstance.value(forKey: #keyPath(BookV2.frontCover)) as? Cover else {
       return
     }
 
     guard let book = manager.destinationInstances(forEntityMappingName: mapping.name, sourceInstances: [sInstance]).first else {
-      fatalError("must return book") }
+      fatalError("must return book")
+    }
 
     guard let context = book.managedObjectContext else {
       fatalError("must have context")
     }
 
+    print("⚠️", context.registeredObjects.count)
+
     let cover = NSEntityDescription.insertNewObject(forEntityName: "Cover", into: context)
     cover.setValue(frontCover.text.data(using: .utf8), forKey: #keyPath(CoverV3.data))
     cover.setValue(book, forKey: #keyPath(CoverV3.book))
+  }
+
+  override func createRelationships(forDestination dInstance: NSManagedObject, in mapping: NSEntityMapping, manager: NSMigrationManager) throws {
+    try super.createRelationships(forDestination: dInstance, in: mapping, manager: manager)
+    // In a properly designed data model, this method will rarely, if ever, be needed.
+    // The intention of this method (which is called in the second pass) is to build any relationships for the new destination entity
+    // that was created in the previ- ous method. However, if all the relationships in the model are double-sided, this method is not necessary
+    // because we already set up one side of them.
   }
 }
