2c795e9098b4179eb6edb431b4808f64a104d277
Alessandro Marzoli <alessandromarzoli@gmail.com>
2022-03-23 17:00:21 +0100

    Swift 5.6 and docs

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.6
 
 import PackageDescription
 import Foundation
@@ -37,6 +37,9 @@ let package = Package(
   platforms: [.macOS(.v10_14), .iOS(.v12), .tvOS(.v12), .watchOS(.v5)],
   products: [
     .library(name: "CoreDataPlus", targets: ["CoreDataPlus"])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
   ],
   targets: [
     .target(name: "CoreDataPlus", path: "Sources"),
