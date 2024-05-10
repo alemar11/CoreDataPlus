8ecba2979c3933afed9e9ec31212e144b1ddd193
Alessandro Marzoli <alessandromarzoli@gmail.com>
2024-05-10 23:24:28 +0200

    --wip-- [skip ci]

---
+++ Tests/Resources/SampleModel3/SampleModel3.swift
@@ -0,0 +1,34 @@
+// CoreDataPlus
+
+import CoreData
+
+// MARK: - V1
+
+@objc(User)
+public class User: NSManagedObject {
+  @NSManaged public var name: String! // unique
+  @NSManaged public var petName: String?
+}
+
+//// MARK: - V2
+//
+//@objc(UserV2)
+//public class UserV2: NSManagedObject {
+//  @NSManaged public var name: String! // unique
+//  @NSManaged public var petName: String?
+//  @NSManaged public var pet: Pet?
+//}
+//
+//@objc(Pet)
+//public class Pet: NSManagedObject {
+//  @NSManaged public var name: String! // unique
+//  @NSManaged public var owner: UserV2!
+//}
+//
+//// MARK: - V2
+//
+//@objc(UserV3)
+//public class UserV3: NSManagedObject {
+//  @NSManaged public var name: String! // unique
+//  @NSManaged public var pet: Pet?
+//}
--- Tests/StagedMigrations_Tests.swift
+++ Tests/StagedMigrations_Tests.swift
@@ -3,7 +3,43 @@
 import CoreData
 import XCTest
 
-final class StagedMigrations_Tests: BaseTestCase {
+@available(iOS 17.0, tvOS 17.0, watchOS 10.0, macOS 14.0, visionOS 1.0, iOSApplicationExtension 17.0, macCatalystApplicationExtension 17.0, *)
+final class StagedMigrations_Tests: XCTestCase {
+  @available(iOS 17.0, tvOS 17.0, watchOS 10.0, macOS 14.0, visionOS 1.0, iOSApplicationExtension 17.0, macCatalystApplicationExtension 17.0, *)
+//  func test_yo() {
+//    print(SampleModelVersion3.version1.managedObjectModel().versionChecksum)
+//    print(SampleModelVersion3.version2.managedObjectModel().versionChecksum)
+//  }
+  
+//  func test_generateSample() throws {
+//    let container = NSPersistentContainer(name: "SampleModel3",
+//                                          managedObjectModel: SampleModelVersion3.version1.managedObjectModel())
+//    
+//    let url = URL.newDatabaseURL(withID: UUID())
+//    
+//    let description = NSPersistentStoreDescription()
+//    description.url = url
+//    container.persistentStoreDescriptions = [description]
+//
+//    
+//    container.loadPersistentStores { description, error in
+//      XCTAssertNil(error)
+//    }
+//    
+//    let context = container.viewContext
+//    
+//    for x in 0..<3 {
+//      for i in 0..<4 {
+//        let user = User(context: context)
+//        user.name = "User_\(x)"
+//        user.petName = "Dog_\(x)_\(i)"
+//      }
+//    }
+//    
+//    try context.save()
+//    
+//    print(url.path())
+//  }
 
 //  @available(iOS 17.0, tvOS 17.0, watchOS 10.0, macOS 14.0, visionOS 1.0, iOSApplicationExtension 17.0, macCatalystApplicationExtension 17.0, *)
 //  func test_MigrationFromV1ToV2() throws {
@@ -55,57 +91,77 @@ final class StagedMigrations_Tests: BaseTestCase {
 //    try NSPersistentStoreCoordinator.destroyStore(at: sourceURL)
 //  }
   
-//  @available(iOS 17.0, tvOS 17.0, watchOS 10.0, macOS 14.0, visionOS 1.0, iOSApplicationExtension 17.0, macCatalystApplicationExtension 17.0, *)
-//  func test_MigrationFromV1ToV2() throws {
-//    let sourceURL = try createSQLiteSample2ForV1()
-//    let steps = SampleModel2.SampleModel2Version.version1.stagedMigrationSteps(to: .version2)
-//    XCTAssertEqual(steps.count, 1)
-//    
-//    let version = try SampleModel2.SampleModel2Version(persistentStoreURL: sourceURL)
-//    XCTAssertTrue(version == .version1)
-//    
-//    let stages = steps.flatMap { $0.stages }
-//    let migrator = NSStagedMigrationManager(stages)
-//    let container = NSPersistentContainer(name: "SampleModel2", 
-//                                          managedObjectModel: SampleModel2.SampleModel2Version.version1.managedObjectModel())
-//    
-//    let storeDescription = try XCTUnwrap(container.persistentStoreDescriptions.first)
-//    storeDescription.url = sourceURL
-//    storeDescription.setOption(migrator, forKey: NSPersistentStoreStagedMigrationManagerOptionKey)
-//    storeDescription.setOption(true as NSNumber, forKey: NSPersistentHistoryTrackingKey)
-//
-//    container.loadPersistentStores { storeDescription, error in
-//      if let error = error {
-//        XCTFail(error.localizedDescription)
-//      }
-//    }
-//    
-//    let migratedContext = container.viewContext
-//    let authors = try AuthorV2.fetchObjects(in: migratedContext)
-//    XCTAssertEqual(authors.count, 2)
-//
-////    let cars = try migratedContext.fetch(NSFetchRequest<NSManagedObject>(entityName: "Car"))
-////    XCTAssertTrue(cars.count >= 1)
-////
-//    for author in authors {
-//      for book in author.books {
-//        let b = book as? BookV2
-//        print("🔴", b?.frontCover)
-//      }
-////      if car is LuxuryCar || car is SportCar {
-////        XCTAssertEqual(car.entity.indexes.count, 0)
-////      } else if car is Car {
-////        let index = car.entity.indexes.first
-////        XCTAssertNotNil(index, "There should be a compound index")
-////        XCTAssertEqual(index!.elements.count, 2)
-////      } else {
-////        XCTFail("Undefined")
-////      }
-//    }
-//
-//    migratedContext._fix_sqlite_warning_when_destroying_a_store()
-//    try NSPersistentStoreCoordinator.destroyStore(at: sourceURL)
-//  }
+  @available(iOS 17.0, tvOS 17.0, watchOS 10.0, macOS 14.0, visionOS 1.0, iOSApplicationExtension 17.0, macCatalystApplicationExtension 17.0, *)
+  func test_MigrationFromV1ToV2() throws {
+    let sourceURL = try createSQLiteSample3ForV1()
+    let steps = SampleModelVersion3.version1.stagedMigrationSteps(to: .version2)
+    XCTAssertEqual(steps.count, 1)
+    
+    let version = try SampleModelVersion3(persistentStoreURL: sourceURL)
+    XCTAssertTrue(version == .version1)
+    
+    let stages = steps.flatMap { $0.stages }
+    let migrator = NSStagedMigrationManager(stages)
+    let container = NSPersistentContainer(name: "SampleModel3",
+                                          managedObjectModel: SampleModelVersion3.version2.managedObjectModel())
+    
+    let storeDescription = try XCTUnwrap(container.persistentStoreDescriptions.first)
+    storeDescription.url = sourceURL
+    storeDescription.setOption(migrator, forKey: NSPersistentStoreStagedMigrationManagerOptionKey)
+
+    container.loadPersistentStores { storeDescription, error in
+      if let error = error {
+        XCTFail(error.localizedDescription)
+      }
+    }
+    
+    let migratedContext = container.viewContext
+    let users = try migratedContext.fetch(NSFetchRequest<NSManagedObject>(entityName: "User"))
+    for user in users {
+      let pet = user.value(forKey: "pet") as? NSManagedObject
+      let petName = user.value(forKey: "petName") as? String
+      XCTAssertNotNil(pet)
+      XCTAssertNotNil(petName)
+      XCTAssertEqual(pet?.value(forKey: "name") as? String, petName)
+    }
+  
+    migratedContext._fix_sqlite_warning_when_destroying_a_store()
+    try NSPersistentStoreCoordinator.destroyStore(at: sourceURL)
+  }
+  
+  func test_MigrationFromV1ToV3() throws {
+    let sourceURL = try createSQLiteSample3ForV1()
+    let steps = SampleModelVersion3.version1.stagedMigrationSteps(to: .version3)
+    XCTAssertEqual(steps.count, 2)
+    
+    let version = try SampleModelVersion3(persistentStoreURL: sourceURL)
+    XCTAssertTrue(version == .version1)
+    
+    let stages = steps.flatMap { $0.stages }
+    let migrator = NSStagedMigrationManager(stages)
+    let container = NSPersistentContainer(name: "SampleModel3",
+                                          managedObjectModel: SampleModelVersion3.version3.managedObjectModel())
+    
+    let storeDescription = try XCTUnwrap(container.persistentStoreDescriptions.first)
+    storeDescription.url = sourceURL
+    storeDescription.setOption(migrator, forKey: NSPersistentStoreStagedMigrationManagerOptionKey)
+
+    container.loadPersistentStores { storeDescription, error in
+      if let error = error {
+        XCTFail(error.localizedDescription)
+      }
+    }
+    
+    let migratedContext = container.viewContext
+    let users = try migratedContext.fetch(NSFetchRequest<NSManagedObject>(entityName: "User"))
+    for user in users {
+      let pet = user.value(forKey: "pet") as? NSManagedObject
+      XCTAssertNotNil(pet)
+    }
+
+    migratedContext._fix_sqlite_warning_when_destroying_a_store()
+    try NSPersistentStoreCoordinator.destroyStore(at: sourceURL)
+  }
 
 //  @available(iOS 17.0, tvOS 17.0, watchOS 10.0, macOS 14.0, visionOS 1.0, iOSApplicationExtension 17.0, macCatalystApplicationExtension 17.0, *)
 //  func test_MigrationFromV2ToV3() throws {
@@ -146,25 +202,37 @@ final class StagedMigrations_Tests: BaseTestCase {
 
 }
 
-func createSQLiteSample2ForV1() throws -> URL {
+//func createSQLiteSample2ForV1() throws -> URL {
+//  let bundle = Bundle.tests
+//  let _sourceURL = try XCTUnwrap(bundle.url(forResource: "SampleModel2V1", withExtension: "sqlite"))
+//
+//  // Being the test run multiple times, we create an unique copy for every test
+//  let uuid = UUID().uuidString
+//  let sourceURL = bundle.bundleURL.appendingPathComponent("SampleModel2V1_copy-\(uuid).sqlite")
+//  try FileManager.default.copyItem(at: _sourceURL, to: sourceURL)
+//  XCTAssertTrue(FileManager.default.fileExists(atPath: sourceURL.path))
+//  return sourceURL
+//}
+//
+//func createSQLiteSample2ForV2() throws -> URL {
+//  let bundle = Bundle.tests
+//  let _sourceURL = try XCTUnwrap(bundle.url(forResource: "SampleModel2V2", withExtension: "sqlite"))
+//
+//  // Being the test run multiple times, we create an unique copy for every test
+//  let uuid = UUID().uuidString
+//  let sourceURL = bundle.bundleURL.appendingPathComponent("SampleModel2V2_copy-\(uuid).sqlite")
+//  try FileManager.default.copyItem(at: _sourceURL, to: sourceURL)
+//  XCTAssertTrue(FileManager.default.fileExists(atPath: sourceURL.path))
+//  return sourceURL
+//}
+
+func createSQLiteSample3ForV1() throws -> URL {
   let bundle = Bundle.tests
-  let _sourceURL = try XCTUnwrap(bundle.url(forResource: "SampleModel2V1", withExtension: "sqlite"))
+  let _sourceURL = try XCTUnwrap(bundle.url(forResource: "SampleModel3_V1", withExtension: "sqlite"))
 
   // Being the test run multiple times, we create an unique copy for every test
   let uuid = UUID().uuidString
-  let sourceURL = bundle.bundleURL.appendingPathComponent("SampleModel2V1_copy-\(uuid).sqlite")
-  try FileManager.default.copyItem(at: _sourceURL, to: sourceURL)
-  XCTAssertTrue(FileManager.default.fileExists(atPath: sourceURL.path))
-  return sourceURL
-}
-
-func createSQLiteSample2ForV2() throws -> URL {
-  let bundle = Bundle.tests
-  let _sourceURL = try XCTUnwrap(bundle.url(forResource: "SampleModel2V2", withExtension: "sqlite"))
-
-  // Being the test run multiple times, we create an unique copy for every test
-  let uuid = UUID().uuidString
-  let sourceURL = bundle.bundleURL.appendingPathComponent("SampleModel2V2_copy-\(uuid).sqlite")
+  let sourceURL = bundle.bundleURL.appendingPathComponent("SampleModel3_V1_copy-\(uuid).sqlite")
   try FileManager.default.copyItem(at: _sourceURL, to: sourceURL)
   XCTAssertTrue(FileManager.default.fileExists(atPath: sourceURL.path))
   return sourceURL
